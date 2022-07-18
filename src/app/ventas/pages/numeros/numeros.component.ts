3af7a679fc41df302882cfc21c13523cda69ea10
Zett0x <zetto1514@gmail.com>
2022-07-18 17:43:45 +0200

    a

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
   styleUrls: ['./numeros.component.css']
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent{
+
+  ventasNetas:number=25654444.5567;
+  porcentaje:number=0.48;
+
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+
 
 }
